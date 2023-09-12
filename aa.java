520c11943ba30e3bee6ab6ffda6c53dc2d4d8387
FernandoAraujoo 1 <fernando.dev.tech@gmail.com>
2023-09-12 15:11:05 +0000

    2

--- aa.java
+++ aa.java
@@ -4,13 +4,21 @@ class aa {
         System.out.println("Digite o a quantidade de digitos do array");
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] vet= new int(N);
+        int vet[] = new int[N];
         System.out.println("Digite "+ N +" números: ");
+        float media = 0;
+        int soma =0;
         
-        for(i=0 ; i<N; i++){
+        for(int i=0 ; i<N; i++){
            vet[i] = sc.nextInt();
-            System.out.print(vet[i]);
+            System.out.println(" valor "+i+ " array: " + vet[i]);
+            soma = vet[i] + soma;
         }
+        float soma1 = soma;
+        float N1 = N;
+        media = soma1/N1;
+        System.out.println("soma: " + soma);
+        System.out.println("resultado: "+ media);
         sc.close();
     }
 
